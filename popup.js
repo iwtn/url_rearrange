2ee4bc5d19706b38e7e70b627908fcbd6a89dd38
iwtn <ganta.viii@gmail.com>
2019-03-04 17:26:52 +0900

    copy url

--- popup.js
+++ popup.js
@@ -1,5 +1,13 @@
 const onInit = _ => {
-  console.log('hoge');
+  chrome.tabs.query({ active: true, currentWindow: true, lastFocusedWindow: true }, function (tabs) {
+    const url = tabs[0].url;
+
+    let copyTarget = document.querySelector("#copy-target");
+    copyTarget.textContent = url;
+    copyTarget.select();
+    document.execCommand('copy');
+  });
 }
+
 
 document.addEventListener("DOMContentLoaded", onInit);
