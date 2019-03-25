dfbb76b106b00de8621c84c5aba8bdcf766fee58
iwtn <ganta.viii@gmail.com>
2019-03-25 22:21:20 +0900

    add a protocol part

--- popup.js
+++ popup.js
@@ -1,13 +1,13 @@
 const urlPartKinds = [
   // 'href',
   // 'host',
+  { name: 'protocol',     delimiter: '',  default: true },
   { name: 'hostname',     delimiter: '',  default: true },
   { name: 'port',         delimiter: ':', default: false },
   { name: 'pathname',     delimiter: '/', default: false },
   { name: 'hash',         delimiter: '#', default: false },
   { name: 'searchParams', delimiter: '&', default: false },
   // 'password',
-  // 'protocol',
   // 'search',
   // 'username'
 ];
