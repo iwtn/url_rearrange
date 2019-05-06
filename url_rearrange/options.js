8a5742c7a24a3c232ec8500a9a651ddbb9902d27
iwtn <ganta.viii@gmail.com>
2019-05-06 23:56:19 +0900

    add a dummy setting

--- url_rearrange/options.js
+++ url_rearrange/options.js
@@ -1,4 +1,17 @@
+const dummyList = [
+  {
+    protocol: 'https',
+    hostname: 'example.com',
+    port:     undefined,
+    pathname: ['hoge', 'fuga'],
+    hash:     undefined,
+    searchParams: { 'one': 1},
+  },
+];
+
 const view = () => {
+  localStorage["urlSettings"] = dummyList;
+
   const list = localStorage["urlSettings"];
   const tag = document.querySelector('#settings');
   if (list == undefined) {
