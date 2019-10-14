5c65321bf8078426b4f5181bde2b18cb1c85146b
iwtn <ganta.viii@gmail.com>
2019-10-14 23:28:30 +0900

    add a watch option

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/popup.js',
+  watch: true,
   output: {
     filename: 'popup.js',
     path: path.resolve(__dirname, 'url_rearrange'),
