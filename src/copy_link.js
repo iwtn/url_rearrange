873ad5e9f1aedeed5410d16f9b34cbcf3443946e
iwtn <ganta.viii@gmail.com>
2019-10-26 23:58:06 +0900

    use template tag

--- src/copy_link.js
+++ src/copy_link.js
@@ -1,22 +1,10 @@
 import copy from './copy';
 
-const template = document.createElement('template');
-const fragment = document.createDocumentFragment();
-
-const div = document.createElement('div');
-div.innerHTML = 'hoge';
-fragment.appendChild(div);
-template.innerHTML = '<div>hoge</div>';
-
-template.style = `
-color: red;
-`;
-
 
 class CopyLink extends HTMLElement {
   constructor() {
     super();
-    // ShadowDOMを使った実装が入る
+    const template = document.getElementById('copy-link');
   }
 }
 
